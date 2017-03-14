b8a7360e89b03f876bfdfe42d9fed8d56fabca2b
Santiago Jorda Ramos <sjorda@alumni.afi.es>
2017-03-14 16:07:02 +0100

    anadido laberinto y creado clase

---
+++ solver.py
@@ -0,0 +1,25 @@
+
+class LaberinthSolver(object):
+
+    def __init__(self, filePath):
+        pass
+
+
+    def loadFile(self):
+        """
+        Devolver una matriz 2x2 con el mapa
+        """
+        pass
+
+
+    def countMoves(self):
+        """
+        Devolver cuantos movimientos nos hacen falta para salir del alberinto
+        """
+        pass
+
+    def solve(self):
+        """
+        Devolver la sequencia de movientos para salir del mapa
+        """
+        pass
